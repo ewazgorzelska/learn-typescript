a1f5ebd7231f2940165d9d457bfcac945daf717f
Ewa Zgorzelska <ewa.naroznik@hotmail.com>
2021-11-07 14:12:15 +0100

    setting my first interface

--- index.ts
+++ index.ts
@@ -1,7 +1,49 @@
-const firstName: string = "John";
-const lastName: string = 'Doe';
-const age: number = 29;
-const email: string = "john.doe@com";
-const language: string = "C#";
-const proficiency: string = 'expert';
-const hobbies: string[] = ['sport', 'netflix', 'books'];
+enum Proficiency {
+    Junior = 'junior',
+    Regular = 'regular',
+    Expert = 'expert'
+};
+
+interface Candidate {
+    firstName: string,
+    lastName: string,
+    age: number,
+    email: string,
+    mainLanguage: string,
+    proficiency: Proficiency,
+    hobbies: string[]
+}
+
+const candidate = {
+    firstName: "John",
+    lastName: 'Doe',
+    age: 29,
+    email: "john.doe@com",
+    mainLanguage: "C#",
+    proficiency: Proficiency.Expert,
+    hobbies: ['sport', 'netflix', 'books']
+}
+
+function getName(candidate: Candidate): string {
+    return `${candidate.firstName} ${candidate.lastName}`;
+};
+
+function processCandidate(candidate: Candidate) {
+    const fullName = getName(candidate);
+    console.log('hello, ' + fullName + '!');
+
+    if (candidate.email == null) {
+        console.log('We cannot process your resume because email is missing');
+        return;
+    }
+    if (candidate.proficiency === Proficiency.Junior || candidate.proficiency === Proficiency.Regular) {
+        console.log('Unfortunately right now we\'re looking for ' + candidate.mainLanguage + ' experts :(');
+        return;
+    }
+    if (candidate.age.length < 2) {
+        console.log('You are too young');
+    }
+    console.log('Your resume is being processed');
+};
+
+processCandidate(candidate)
