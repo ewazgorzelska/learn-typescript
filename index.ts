987bf0d778ac22a076f4eb25619b4bd86dab7781
Ewa Zgorzelska <ewa.naroznik@hotmail.com>
2021-11-07 13:14:01 +0100

    setting my first types

---
+++ index.ts
@@ -0,0 +1,7 @@
+const firstName: string = "John";
+const lastName: string = 'Doe';
+const age: number = 29;
+const email: string = "john.doe@com";
+const language: string = "C#";
+const proficiency: string = 'expert';
+const hobbies: string[] = ['sport', 'netflix', 'books'];
